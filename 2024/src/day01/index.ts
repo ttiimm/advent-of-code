e931daa933064d8f2e9e5c7f670e6155175404d9
Tim Likarish Ellis <likarish@gmail.com>
2024-12-02 21:14:21 -0500

    [2024 - Day 1] Solution
    Uses input stored in 2024/input/day01/input.txt and outputs the distance and similarity
    between the two lists.

--- 2024/src/day01/index.ts
+++ 2024/src/day01/index.ts
@@ -1,5 +1,62 @@
-function hello(): string {
-    return `Hello, World!`;
+class Inputs {
+    left: number[] = [];
+    right: number[] = [];
 }
 
-console.log(hello());
+async function _readInputs(): Promise<Inputs> {
+    const inputs = new Inputs();
+    const fs = require('fs');
+    const readline = require('readline');
+    const fileStream = fs.createReadStream("./input/day01/input.txt");
+    const rl = readline.createInterface({
+        input: fileStream,
+    });
+
+    for await (const line of rl) {
+        const [first, second] = line.split("   ");
+        inputs.left.push(first);
+        inputs.right.push(second);
+    }
+    // inputs.left = [3, 4, 2, 1, 3, 3];
+    // inputs.right = [4, 3, 5, 3, 9, 3];
+    return inputs;
+}
+
+function _findDistances(inputs: Inputs): number[] {
+    const leftSorted = inputs.left.sort();
+    const rightSorted = inputs.right.sort();
+    return leftSorted.map((n, i) => Math.abs(n - rightSorted[i]));
+}
+
+function _findSimilarities(inputs: Inputs): number[] {
+    const similarities: number[] = [];
+    for (const n of inputs.left) {
+        const freq = inputs.right.filter(r => n == r);
+        const count = freq.length;
+        similarities.push(n * count);
+    }
+    return similarities;
+}
+
+
+async function distance(): Promise<number> {
+    const inputs = await _readInputs();
+    const distances = _findDistances(inputs);
+    const summed = distances.reduce((sum, n) => sum + n, 0);
+    return summed;
+}
+
+async function similaritiy(): Promise<number> {
+    const inputs = await _readInputs();
+    const similarities = _findSimilarities(inputs);
+    const summed = similarities.reduce((sum, n) => sum + n, 0);
+    return summed;
+}
+
+distance().then(result => {
+    console.log(result);
+})
+
+similaritiy().then(result => {
+    console.log(result);
+})
